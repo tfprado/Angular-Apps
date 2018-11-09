bb8ff53cc218248d68d62030d2666fd62781a978
Thiago Prado <tfprado2@gmail.com>
2018-11-09 16:09:00 -0500

    added country angular program

--- country-app/src/app/app.module.ts
+++ country-app/src/app/app.module.ts
@@ -3,15 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CountryService } from './country.service';
+import { HomeComponent } from './home/home.component';
+import { AllCountriesComponent } from './all-countries/all-countries.component';
+import { CountryDetailComponent } from './country-detail/country-detail.component';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AllCountriesComponent,
+    CountryDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [CountryService],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
